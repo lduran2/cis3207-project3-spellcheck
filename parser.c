0918cd32146da7ac4afb1c24fff829446eea8855
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-03-20 23:06:52 -0400

    brought shell's promptline into parser

--- parser.c
+++ parser.c
@@ -239,3 +239,57 @@ Queue
 
 } /* end *process_splitter(char**) */
 
+/**
+ * Wraps the getline method by giving the user a prompt first.
+ * @params
+ *   *prompt   : char = displayed to the user before accepting input
+ *   *pargc    : int  = pointer to argument count
+ *   ***pargv  : char = pointer to parsed argument values
+ *                         (NULL terminated)
+ *   *success  : bool = whether parsing was successful
+ *   *instream : FILE = the input stream
+ * @returns true if no exit code; false on exit code.
+ */
+bool
+promptline(
+	char* prompt, int *pargc, char ***pargv, bool *success,
+	FILE *instream)
+{
+	char *line = NULL;	/* line read in */
+	char *raw_line = NULL;	/* line read in, raw from getline */
+	size_t n = 0;	/* line length */
+
+	/* prompt and read line */
+	fprintf(stderr, "%s", prompt);	/* write the prompt */
+	fflush(stderr); /* flush the stderr buffer */
+	/* accept a new raw line */
+	getline(&raw_line, &n, stdin);
+
+	/* get the length removing the line feed '\n' */
+	n = (strlen(raw_line) - 1);
+	/* Ctrl+D: shortcut for exit */
+	if (0 == n) { /* the user pressed Ctrl+D */
+		*success = true;	/* report success */
+		printf("exit\n");	/* show exit */
+		return false;	/* and exit */
+	} /* if (0 == n) */
+
+	/* properly terminate the line */
+	line = malloc((n + 1) * sizeof(char));
+	strncpy(line, raw_line, n);
+	strcat(line, "\0");
+
+	/* parse the line, but on error: */
+	*success = parse(line, pargc, pargv); /* report if success */
+	if (!*success) {
+		return true;	/* do NOT exit on error though */
+	} /* if (!*success) */
+
+	/* continue conditions */
+	return (
+		/* user did typed the exit command */
+		(0 != strcmp(**pargv, "exit"))
+		/* user did typed the quit command */
+		&& (0 != strcmp(**pargv, "quit"))
+	);
+} /* end promptline(char*, char**, size_t*, bool*, FILE*) */
